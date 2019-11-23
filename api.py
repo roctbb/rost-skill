5b61e374482c0f45af62eb2241c62f86ac1f8634
roctbb <roctbb@gmail.com>
2019-11-23 20:28:18 +0300

    Initial commit

---
+++ api.py
@@ -0,0 +1,74 @@
+# coding: utf-8
+# Импортирует поддержку UTF-8.
+from __future__ import unicode_literals
+
+# Импортируем модули для работы с JSON и логами.
+import json
+import logging
+
+# Импортируем подмодули Flask для запуска веб-сервиса.
+from flask import Flask, request
+app = Flask(__name__)
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Хранилище данных о сессиях.
+sessionStorage = {}
+
+# Задаем параметры приложения Flask.
+@app.route("/", methods=['POST'])
+
+def main():
+# Функция получает тело запроса и возвращает ответ.
+    logging.info('Request: %r', request.json)
+
+    response = {
+        "version": request.json['version'],
+        "session": request.json['session'],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    handle_dialog(request.json, response)
+
+    logging.info('Response: %r', response)
+
+    return json.dumps(
+        response,
+        ensure_ascii=False,
+        indent=2
+    )
+
+# Функция для непосредственной обработки диалога.
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        # Это новый пользователь.
+        # Инициализируем сессию и поприветствуем его.
+
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
+            ]
+        }
+
+        res['response']['text'] = 'Я вас категорически приветствую'
+        return
+    else:
+        # Обрабатываем ответ пользователя.
+        if req['request']['original_utterance'].lower() in [
+            'анек', 'анекдот'
+        ]:
+            # Пользователь согласился, прощаемся.
+            res['response']['text'] = 'Купил мужик шляпу, а она ему как раз!'
+            return
+
+        # Если нет, то убеждаем его купить слона!
+        res['response']['text'] = 'Не очень понял, к сожалению...'
+
+app.run(debug=False, host='0.0.0.0', port=5000, ssl_context='adhoc')
